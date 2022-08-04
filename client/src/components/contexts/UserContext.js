6b219c347ea2b85e85cf8643961c4778970fd79d
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-04 15:30:13 +0300

    Create UserContext.js

---
+++ client/src/components/contexts/UserContext.js
@@ -0,0 +1,32 @@
+import { createContext } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useLocalStorage("token", {});
+
+  const userLogin = (authData) => {
+      console.log(authData, 'authData from userContext, row 10');
+    setAuth(authData);
+  };
+
+  const userLogout = () => {
+    setAuth({});
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user: auth,
+        userLogin,
+        userLogout,
+        isAuthenticated: !!auth.accessToken,
+      }}
+    >
+      {" "}
+      //!! converts object to boolean; alternatively Boolean(accessToken)
+      {children}
+    </AuthContext.Provider>
+  );
+};
