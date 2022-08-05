f1f8b5337aff0153982158cc32886e41f4071506
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-05 16:38:53 +0300

    Created single hive component

---
+++ client/src/components/hives/Hive.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+export const Hive = (props) => {
+  return (
+    <div className="hive">
+      <div className="hive-img">
+        <img src="/pictures/hive2.jpg" alt="hive" />
+      </div>
+      <div className="hive-info">
+        <h1>{props.hive.number}</h1>
+        <p>
+          <span>Location: </span>
+          {props.hive.location}
+        </p>
+        <p>
+          <span>Owner: </span>{props.hive._ownerId.username}
+        </p>
+      </div>
+
+      <Link to="/hives/details" className="btn-details">
+        Details
+      </Link>
+    </div>
+  );
+};
