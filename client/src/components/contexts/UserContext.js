b69a200ebfed18e6765cd9044390b425df7de5f5
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-05 11:54:47 +0300

    Removed unneeded console.log on row 10

--- client/src/components/contexts/UserContext.js
+++ client/src/components/contexts/UserContext.js
@@ -7,7 +7,6 @@ export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useLocalStorage("token", {});
 
   const userLogin = (authData) => {
-      console.log(authData, 'authData from userContext, row 10');
     setAuth(authData);
   };
 
