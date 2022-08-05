8c2083511855cd7b8f4197cda3b43fa64a25f937
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-05 15:06:41 +0300

    Some navigation bar repositioning

--- client/src/components/common/Navigation.js
+++ client/src/components/common/Navigation.js
@@ -1,6 +1,11 @@
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+
+import { AuthContext } from "../contexts/UserContext";
 
 export const Navigation = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <header>
       <nav>
@@ -21,45 +26,64 @@ export const Navigation = () => {
               All Hives
             </NavLink>
           </li>
+
+          {user._id ? (
+            <>
+              <li>
+                <NavLink
+                  to="/hives/create"
+                  className={({ isActive }) =>
+                    isActive ? "active" : undefined
+                  }
+                >
+                  Create Hive
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/users/logout"
+                  className={({ isActive }) =>
+                    isActive ? "active" : undefined
+                  }
+                >
+                  Logout
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink
+                  to="/users/login"
+                  className={({ isActive }) =>
+                    isActive ? "active" : undefined
+                  }
+                >
+                  Login
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/users/register"
+                  className={({ isActive }) =>
+                    isActive ? "active" : undefined
+                  }
+                >
+                  Register
+                </NavLink>
+              </li>
+            </>
+          )}
+        </ul>
+        <ul>
           <li>
             <NavLink
               to="/hives/search"
-              className={({ isActive }) => (isActive ? "active" : undefined)}
+              className={({ isActive }) =>
+                isActive ? "active search" : "search"
+              }
             >
               Search
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/hives/create"
-              className={({ isActive }) => (isActive ? "active" : undefined)}
-            >
-              Create Hive
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              to="/users/login"
-              className={({ isActive }) => (isActive ? "active" : undefined)}
-            >
-              Login
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/users/register"
-              className={({ isActive }) => (isActive ? "active" : undefined)}
-            >
-              Register
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/users/logout"
-              className={({ isActive }) => (isActive ? "active" : undefined)}
-            >
-              Logout
             </NavLink>
           </li>
         </ul>
