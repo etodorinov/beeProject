73e34ebfe133b00ec2f9355c5018988e597f5943
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-09 16:00:57 +0300

    Change links for hive notes

--- client/src/App.js
+++ client/src/App.js
@@ -52,7 +52,7 @@ function App() {
             }
           />
           <Route
-            path="/hives/notes/:id"
+            path="/hives/notes/:id/view"
             element={
               <HiveProvider>
                 <Notes />
@@ -60,7 +60,7 @@ function App() {
             }
           />
           <Route
-            path="/hives/add-note/:id"
+            path="/hives/notes/:id/add"
             element={
               <HiveProvider>
                 <AddNotes />
