f594590185ddc21c9ac4c03a262a5a7079b6da2f
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-09 13:36:24 +0300

    Added logic to confirm the deletion of the hive

--- client/src/components/hives/DeleteHive.js
+++ client/src/components/hives/DeleteHive.js
@@ -1,21 +1,62 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { confirmAlert } from "react-confirm-alert";
+import { useState, useEffect } from "react";
 
-import { removeHive } from "../../services/itemsServices";
+import { getOne, removeHive } from "../../services/itemsServices";
 
 export const DeleteHive = () => {
+  const [hive, setHive] = useState();
   const location = useLocation();
   const navigate = useNavigate();
 
   const hiveId = location.pathname.split("/").pop();
 
   useEffect(() => {
-    removeHive(hiveId)
-      .then(() => {
-        navigate("/hives/catalogue");
-      })
-      .catch(() => {
-        navigate("*");
+    getOne(hiveId)
+      .then((response) => response.json())
+      .then((result) => {
+        setHive(result);
       });
+  }, []);
+
+  confirmAlert({
+    customUI: ({ onClose }) => {
+      return (
+        <div className="overlay">
+          <div className="delete-confirmation">
+            <h1>Are you sure you want to delete this hive?</h1>
+            <div className="hive-text">
+              <h3 id="name">Name: {hive.number}</h3>
+              <h3 id="location">Location: {hive.location}</h3>
+            </div>
+            <button
+              className="delete-confirmation-yes"
+              onClick={() => {
+                removeHive(hiveId)
+                  .then(() => {
+                    navigate("/hives/catalogue");
+                    onClose();
+                  })
+                  .catch(() => {
+                    navigate("*");
+                    onClose();
+                  });
+              }}
+            >
+              Yes
+            </button>
+            <button
+              className="delete-confirmation-no"
+              onClick={() => {
+                navigate(`/hives/details/${hiveId}`);
+                onClose();
+              }}
+            >
+              No
+            </button>
+          </div>
+        </div>
+      );
+    },
   });
 };
