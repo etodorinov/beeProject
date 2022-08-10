69007a37da9126041f6590b5b82d58db7c107d15
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-10 10:29:23 +0300

    Added functionality to add the note to its creator

--- server/src/services/noteService.js
+++ server/src/services/noteService.js
@@ -1,10 +1,12 @@
 const Hive = require("../models/HiveModel");
 const Note = require("../models/NoteModel");
+const User = require("../models/UserModel");
 
 async function createNote(data) {
   const { hiveId, date, note } = { ...data };
 
   const hive = await Hive.findById(hiveId);
+  const user = await User.findById(hive._ownerId);
 
   const createdNote = await Note.create({
     date,
@@ -15,6 +17,9 @@ async function createNote(data) {
 
   hive.notes.push(createdNote);
   hive.save();
+
+  user.notesCreated.push(createdNote);
+  user.save();
 
   return createdNote;
 }
