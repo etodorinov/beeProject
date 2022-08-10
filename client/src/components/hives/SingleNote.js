c78ec17789d34c1045804af769771c83e2cb4485
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-10 15:17:53 +0300

    Removed the outline on row 3 so all notes go in the same div

--- client/src/components/hives/SingleNote.js
+++ client/src/components/hives/SingleNote.js
@@ -1,12 +1,10 @@
 export const SingleNote = (props) => {
   return (
-    <div className="note-list">
-      <div className="note">
-        <div className="note-info">
-          <h1>Date: {props.note.date}</h1>
-          <span>Note:</span>
-          <p>{props.note.note}</p>
-        </div>
+    <div className="note">
+      <div className="note-info">
+        <h1>Date: {props.note.date}</h1>
+        <span>Note:</span>
+        <p>{props.note.note}</p>
       </div>
     </div>
   );
