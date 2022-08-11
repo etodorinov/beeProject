6e185b1d31959e330e47bd33516a745fff7110e3
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-11 22:45:33 +0300

    Two ul merged

--- client/src/components/common/Navigation.js
+++ client/src/components/common/Navigation.js
@@ -75,8 +75,6 @@ export const Navigation = () => {
               </li>
             </>
           )}
-        </ul>
-        <ul>
           <li>
             <NavLink
               to="/hives/search"
