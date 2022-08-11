bb12f454dc3e37b475d9f4beeafcbef11068d7bf
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-11 23:44:09 +0300

    Some route guards edit

--- client/App.js
+++ client/App.js
@@ -8,7 +8,8 @@ import { Login } from "./components/user/Login";
 import { Logout } from "./components/user/Logout";
 import { Navigation } from "./components/common/Navigation";
 import { Footer } from "./components/common/Footer";
-import { RouteGuard } from "./components/common/RouteGuard";
+import { NotLoggedInGuard } from "./components/common/NotLoggedInGuard";
+import { LoggedInGuard } from "./components/common/LoggedInGuard";
 import { Home } from "./components/Home";
 import { Catalogue } from "./components/hives/Catalogue";
 import { Details } from "./components/hives/Details";
@@ -29,21 +30,71 @@ function App() {
 
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/users/login" element={<Login />} />
-            <Route path="/users/register" element={<Register />} />
             <Route path="/hives/catalogue" element={<Catalogue />} />
             <Route path="/hives/details/:id" element={<Details />} />
             <Route path="/hives/search" element={<Search />} />
             <Route path="/notes/:id/view" element={<Notes />} />
             <Route path="*" element={<NoSuchLink />} />
 
-            <Route element={<RouteGuard />}>
-              <Route path="/users/logout" element={<Logout />} />
-              <Route path="/hives/create" element={<Create />} />
-              <Route path="/hives/edit/:id" element={<Edit />} />
-              <Route path="/hives/remove/:id" element={<DeleteHive />} />
-              <Route path="/notes/:id/add" element={<AddNotes />} />
-            </Route>
+            <Route
+              path="/users/login"
+              element={
+                <LoggedInGuard>
+                  <Login />
+                </LoggedInGuard>
+              }
+            />
+
+            <Route
+              path="/users/register"
+              element={
+                <LoggedInGuard>
+                  <Register />
+                </LoggedInGuard>
+              }
+            />
+
+            <Route
+              path="/users/logout"
+              element={
+                <NotLoggedInGuard>
+                  <Logout />
+                </NotLoggedInGuard>
+              }
+            />
+            <Route
+              path="/hives/create"
+              element={
+                <NotLoggedInGuard>
+                  RouteGuard <Create />
+                </NotLoggedInGuard>
+              }
+            />
+            <Route
+              path="/hives/edit/:id"
+              element={
+                <NotLoggedInGuard>
+                  <Edit />
+                </NotLoggedInGuard>
+              }
+            />
+
+            <Route
+              path="/hives/remove/:id"
+              element={
+                <NotLoggedInGuard>
+                  <DeleteHive />
+                </NotLoggedInGuard>
+              }
+            />
+            <Route
+              path="/notes/:id/add"
+              element={
+                <NotLoggedInGuard>
+                  <AddNotes />
+                </NotLoggedInGuard>
+              }
+            />
           </Routes>
 
           <Footer />
