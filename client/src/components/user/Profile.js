76bb6772cba5a1dc18e5dc1aba004f4de5973177
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-16 15:17:24 +0300

    Initial commit

---
+++ client/src/components/user/Profile.js
@@ -0,0 +1,47 @@
+import { useContext, useState } from "react";
+
+import { AuthContext } from "../contexts/UserContext";
+
+import { Hive } from "../hives/Hive";
+
+import { getAllByUser } from "../../services/itemsServices";
+
+export const Profile = () => {
+  const [hives, setHives] = useState([]);
+  const { user } = useContext(AuthContext);
+
+  getAllByUser(user._id)
+    .then((response) => response.json())
+    .then((result) => setHives(result));
+
+  const available = hives.length !== 0 ? true : false;
+  console.log(hives);
+  return (
+    <div className="profile">
+      <h4>
+        username: <span>{user.username}</span>
+      </h4>
+      <h4>
+        email: <span>{user.email}</span>
+      </h4>
+      {(available && (
+        <main>
+          <section className="hive-catalogue">
+            {/* <h1>
+              <span>Hives</span>
+            </h1> */}
+            <div className="hive-list">
+              {hives.map((x) => (
+                <Hive key={x._id} hive={x} />
+              ))}
+            </div>
+          </section>
+        </main>
+      )) || (
+        <div className="no-hives">
+          <p>There are no hives found!</p>
+        </div>
+      )}
+    </div>
+  );
+};
