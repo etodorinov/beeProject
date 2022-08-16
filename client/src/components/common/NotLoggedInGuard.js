e6c62aa6dbe5dc3439a672d584d0313c9042f9fb
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-16 21:26:08 +0300

    Add files via upload

--- client/src/components/common/NotLoggedInGuard.js
+++ client/src/components/common/NotLoggedInGuard.js
@@ -1,13 +1,13 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 import { useAuthContext } from "../hooks/useAuthContext";
 
-export const NotLoggedInGuard = ({ children }) => {
+export const NotLoggedInGuard = ({children}) => {
   const { isAuthenticated } = useAuthContext();
-  
+
   if (!isAuthenticated) {
     return <Navigate to="/users/login" replace />;
   }
   
-  return children;
+  return children ? children : <Outlet /> ;
 };
