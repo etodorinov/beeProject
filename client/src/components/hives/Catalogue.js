7e59d990acf7bc66bfdfb5138af9745c33e61747
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-20 09:38:00 +0300

    Loading spinner added

--- client/src/components/hives/Catalogue.js
+++ client/src/components/hives/Catalogue.js
@@ -3,26 +3,40 @@ import { useState, useEffect } from "react";
 import { getAll } from "../../services/itemsServices";
 
 import { Hive } from "./Hive";
+import { LoadingSpinner } from "../common/Spinner";
 
 export const Catalogue = () => {
   let [hives, setHives] = useState([]);
+  let [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     getAll()
-      .then((response) => response.json())
-      .then((result) => setHives(result));
+      .then((response) => {
+        response.json();
+      })
+      .then((result) => {
+        setHives(result);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
   }, []);
 
   hives = hives.sort((a, b) => {
     return a._ownerId?.username.localeCompare(b._ownerId?.username);
   });
+
   const available = hives.length !== 0 ? true : false;
 
   return (
+    (isLoading && <LoadingSpinner />) ||
     (available && (
       <main>
         <section className="hive-catalogue">
           <h1>
-            <span>Hives</span> Trade
+            <span>Hives</span>
           </h1>
           <div className="hive-list">
             {hives.map((x) => (
