f2804961302e024a427d283b3254c671c348cd36
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-11-05 21:35:04 +0200

    Add files via upload

---
+++ server/src/initializations/server.js
@@ -0,0 +1,28 @@
+//former expressinitialization
+
+const express = require("express");
+
+const { PORT } = require("../constants");
+
+const cors = require("../middlewares/corsMiddleware");
+const hiveController = require("../controllers/hiveController");
+const userController = require("../controllers/userController");
+const noteController = require("../controllers/noteController");
+
+function expressInitialization() {
+  const app = express();
+
+  app.use(cors);
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use("/hives", hiveController);
+  app.use("/users", userController);
+  app.use("/notes", noteController);
+
+  app.listen(PORT, () => {
+    console.log(`App started on port ${PORT}...`);
+  });
+}
+
+module.exports = expressInitialization;
